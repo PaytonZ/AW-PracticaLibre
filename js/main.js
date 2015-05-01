ca7fc90417bf0133b4c54f37b91af693711dcc28
PaytoNZ <juanlp01@ucm.es>
2015-05-01 12:20:12 +0200

    subiendo todo , faltaria carrusel pero no es imprecindible

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+/**
+ * Main AngularJS Web Application
+ */
+ var app = angular.module('practicaLibreAW', [
+  'ngRoute'
+  ]);
+
+/**
+ * Configure the Routes
+ */
+ app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+    // Pages
+    .when("/ultrasonidos", {templateUrl: "partials/ultrasonidos.html", controller: "PageCtrl"})
+    .when("/electromagnetico", {templateUrl: "partials/electromagnetico.html", controller: "PageCtrl"})
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+  }]);
+
+
+/**
+ * Controls the pages
+ */
+ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Controlador de Páginas Activado");
+
+  // Activates the Carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+
+});
